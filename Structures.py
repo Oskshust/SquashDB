b8803ad0468d853723b73ec448be926cfe560781
Oskar Szudzik <oszudzik@gmail.com>
2023-06-10 23:06:56 +0200

    Fix bugs, add commands file

--- Structures.py
+++ Structures.py
@@ -20,7 +20,6 @@ class Equipment:
 class CassandraClient:
     def __init__(self, nodes):
         cluster = Cluster(nodes, port=9042)
-        #create_keyspace = "CREATE KEYSPACE IF NOT EXISTS squash WITH REPLICATION={\'class\': \'SimpleStrategy\', \'replication_factor\': 1};"
         self.session = cluster.connect('squash')
 
     def setup(self):
@@ -31,12 +30,17 @@ class CassandraClient:
         create_courts = "CREATE TABLE IF NOT EXISTS courts (court_id int, was_cleaned int, PRIMARY KEY(court_id));"
         self.session.execute(create_courts)
 
+        # reservation_by_start_time = "CREATE MATERIALIZED VIEW reservations_by_time AS SELECT * FROM reservations " \
+        #    "WHERE id IS NOT NULL AND time IS NOT NULL PRIMARY KEY (time)"
+        reservation_by_start_time = "CREATE INDEX IF NOT EXISTS reservations_by_time ON reservations (start_time);"
+        self.session.execute(reservation_by_start_time)
+
         insert_courts = "INSERT INTO courts (court_id, was_cleaned) VALUES (%s, %s);"
         self.session.execute(insert_courts, (1, 1))
         self.session.execute(insert_courts, (2, 1))
         self.session.execute(insert_courts, (3, 0))
 
-        self.sanity_check()
+        # self.sanity_check()
 
     def sanity_check(self):
         sanity_check = "SELECT * FROM courts"
@@ -45,18 +49,37 @@ class CassandraClient:
             print(row)
 
     def create_reservation(self, reservation):
-        query = "INSERT INTO reservations (reservation_id, user_id, equipment, start_time, end_time) VALUES (%s, %s, %s, %s, %s)"
-        self.session.execute(query, (reservation.reservation_id, reservation.user_id, reservation.equipment, reservation.start_time, reservation.end_time))
+        # check if there is free court for this hour
+        occupied_courts = "SELECT * FROM reservations WHERE start_time = %s;"
+        rows = self.session.execute(occupied_courts, reservation.start_time)
+        occ = [row.court_id for row in rows]
 
-    def update_reservation(self, reservation):
+        all_courts_q = "SELECT * FROM courts;"
+        rows = self.session.execute(all_courts_q)
+        all_courts = [row.court_id for row in rows]
+
+        if len(occ) == len(all_courts):
+            print("There is no court left for this hour, try different one, or try again later.")
+
+        else:
+            court_to_be_reserved = list(set(all_courts).difference(occ))[0]
+            query = "INSERT INTO reservations (reservation_id, user_id, court_id, equipment, start_time, end_time) VALUES" \
+                    "(%s, %s, %s, %s, %s, %s)"
+            self.session.execute(query, (reservation.reservation_id, reservation.user_id, court_to_be_reserved, reservation.equipment, reservation.start_time, reservation.end_time))
+
+    def equipment_update_reservation(self, reservation):
         query = "UPDATE reservations SET equipment = %s WHERE reservation_id = %s"
         self.session.execute(query, (reservation.equipment, reservation.reservation_id))
+
+    def update_reservation(self, reservation):
+        self.cancel_reservation(reservation.reservation_id)
+        self.create_reservation(reservation)
 
     def get_reservation(self, reservation_id):
         query = "SELECT * FROM reservations WHERE reservation_id = %s"
         rows = self.session.execute(query, (reservation_id,))
         for row in rows:
-            return Reservation(row.reservation_id, row.user_id, row.equipment, row.start_time, row.end_time)
+            return Reservation(row.reservation_id, row.user_id, row.court_id, row.equipment, row.start_time, row.end_time)
 
     def cancel_reservation(self, reservation_id):
         query = "DELETE FROM reservations WHERE reservation_id = %s"
--- main.py
+++ main.py
@@ -1,24 +1,5 @@
 from Structures import CassandraClient, Reservation
 
-# TODO setting up the cassandra nodes
-# it might be just script in readme or dockerfile or any suitable form really
-
-
-# setting up nodes:
-# docker run --name cas1 -p 9042:9042 -e CASSANDRA_CLUSTER_NAME=MyCluster -d cassandra
-# check IP and set proper one in 'nodes' var
-# docker inspect --format='{{.NetworkSettings.IPAddress}}' cas1
-# docker exeec -it CONTAINER_NAME cqlsh
-# "CREATE KEYSPACE IF NOT EXISTS squash WITH REPLICATION={'class': 'SimpleStrategy', 'replication_factor': 1};"
-# (below) not checked yet
-# docker run --name cas2 -e CASSANDRA_SEEDS="$(docker inspect --format='{{.NetworkSettings.IPAddress}}' cas1)" -e CASSANDRA_CLUSTER_NAME=MyCluster -d cassandra
-
-# TODO making a reservation
-# users decides at which hour he needs a court. Then the reservation runs proper query to check which courts
-# are already taken - query should be probably joined with query on courts to suggest one which is free.
-
-# TODO error handling
-# Write some while loops for user not to crush app with wrong input or updating/canceling reservation which is not there
 
 # TODO stress tests
 # write methods for performing stress tests provided in requirements - 5 tests
@@ -28,7 +9,7 @@ from Structures import CassandraClient, Reservation
 
 def main():
     # connection [WIP]
-    nodes = ['172.17.0.2']
+    nodes = ['172.17.0.2', '172.17.0.3']
     cassandra_client = CassandraClient(nodes)
 
     cassandra_client.setup()
@@ -36,7 +17,7 @@ def main():
     current_reservation_id = 1
 
     is_working = True
-    print("What would you like to do?")
+    print("Welcome in the reservation system for SquashDB. What would you like to do?")
     print("1. Make a reservation")
     print("2. Update a reservation")
     print("3. Check a reservation status")
@@ -46,35 +27,43 @@ def main():
     while(is_working):
         # MAKING A RESERVATION
         if choice == '1':
-            user_id = input("Enter your user ID: ")
+            user_id = int(input("Enter your user ID: "))
             equipment = input("Enter the equipment you would like to reserve: ")
             start_time = input("Enter the start time of your reservation: ")
             end_time = input("Enter the end time of your reservation: ")
-            reservation = Reservation(current_reservation_id, user_id, equipment, start_time, end_time)
+            reservation = Reservation(current_reservation_id, user_id, 0, equipment, start_time, end_time)
             cassandra_client.create_reservation(reservation)
             print(f"Reservation created with ID: {current_reservation_id}")
             current_reservation_id += 1
 
         # UPDATING A RESERVATION
         elif choice == '2':
-            reservation_id = input("Enter the reservation ID: ")
+            reservation_id = int(input("Enter the reservation ID: "))
             equipment = input("Enter the new equipment you would like to reserve: ")
+            new_start = input("Enter the new start time: ")
+            new_end = input("Enter the new end time: ")
             reservation = cassandra_client.get_reservation(reservation_id)
             if reservation:
                 reservation.equipment = equipment
-                cassandra_client.update_reservation(reservation)
+                if new_start == reservation.start_time and new_end == reservation.end_time:
+                    cassandra_client.equipment_update_reservation(reservation)
+                else:
+                    reservation.start_time = new_start
+                    reservation.new_end = new_end
+                    cassandra_client.update_reservation(reservation)
                 print(f"Reservation with ID {reservation_id} updated")
             else:
                 print(f"No reservation found with ID {reservation_id}")
 
         # CHECKING A STATUS
         elif choice == '3':
-            reservation_id = input("Enter the reservation ID: ")
+            reservation_id = int(input("Enter the reservation ID: "))
             reservation = cassandra_client.get_reservation(reservation_id)
             if reservation:
                 print(f"Reservation with ID {reservation_id} details:")
                 print(f"User ID: {reservation.user_id}")
                 print(f"Equipment: {reservation.equipment}")
+                print(f"Court ID: {reservation.court_id}")
                 print(f"Start time: {reservation.start_time}")
                 print(f"End time: {reservation.end_time}")
             else:
@@ -82,7 +71,7 @@ def main():
 
         # CANCEL A RESERVATION
         elif choice == '4':
-            reservation_id = input("Enter the reservation ID: ")
+            reservation_id = int(input("Enter the reservation ID: "))
             cassandra_client.cancel_reservation(reservation_id)
             print(f"Reservation with ID {reservation_id} canceled")
 
@@ -93,7 +82,15 @@ def main():
 
         # WRONG INPUT
         else:
-            choice = input("Enter your choice (1 to 4)")
+            choice = input("Enter your choice (1 to 5): ")
+
+        print("Would you like to do anything else?")
+        print("1. Make a reservation")
+        print("2. Update a reservation")
+        print("3. Check a reservation status")
+        print("4. Cancel a reservation")
+        print("5. Exit the app")
+        choice = input("Enter your choice: ")
 
 
 if __name__ == '__main__':
